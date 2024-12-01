d5d177882af50a850be3027b8fb82c6acfb96fb9
Michael Czolko <michael@czolko.cz>
2024-12-01 16:39:50 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,57 @@
+import kotlin.math.abs
+
+typealias NumberPairs = Pair<List<Int>, List<Int>>
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    /**
+     * Helper functions
+     */
+
+    fun String.toPair(): Pair<Int, Int> {
+        val (a, b) = this.split("   ")
+        return Pair(a.toInt(), b.toInt())
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun Pair<Int, Int>.distance(): Int {
+        return abs(this.second - this.first)
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    fun parseInput(input: String): NumberPairs {
+        val numPairs = readInput(input).map { it.toPair() }
+        return Pair(numPairs.map { it.first }, numPairs.map { it.second })
+    }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    /**
+     * Day 1
+     */
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    fun part1(input: NumberPairs): Int {
+        return input.first.sorted().zip(input.second.sorted()).sumOf { it.distance() }
+    }
+
+    fun part2(input: NumberPairs): Int {
+        return input.first.map { Pair(it, input.second.count { r -> r == it }) }.sumOf { it.first * it.second }
+    }
+
+
+    /**
+     * Tests
+     */
+
+    println("Test Day 1:")
+    val testInput = parseInput("Day01_test")
+    part1(testInput).println()
+    check(part1(testInput) == 11)
+    part2(testInput).println()
+    check(part2(testInput) == 31)
+
+    /**
+     * Show results
+     */
+
+    println("Result Day 1:")
+    val input = parseInput("Day01")
     part1(input).println()
     part2(input).println()
 }
